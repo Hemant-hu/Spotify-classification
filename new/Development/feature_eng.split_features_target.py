f3eb136fdae72d0625502724d70e881646465ff0
Hemant Lad <hemantlad48@gmail.com>
2026-05-04 13:26:57 +0530

    Delete new directory

--- new/Development/feature_eng.split_features_target.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Separate features and target from training data
-X_train = training_df.drop(columns=['id', 'target'])
-y_train = training_df['target']
-
-print(f'X_train shape: {X_train.shape}')
-print(f'y_train shape: {y_train.shape}')
-print(f'Target distribution: {y_train.value_counts().to_dict()}')
